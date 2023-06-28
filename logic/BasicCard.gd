5afd0f2e04ab7229899f83d6d77e8ad77811c43c
LiptonLeon <93220207+LiptonLeon@users.noreply.github.com>
2023-06-28 02:08:56 +0200

    minor changes

--- logic/BasicCard.gd
+++ logic/BasicCard.gd
@@ -1,17 +1,23 @@
 @tool
 class_name BasicCard extends Control
 
+@export var card_icon: Texture
 @export var card_name: String
+@export var card_flavour: String
 @export_multiline var card_text: String
 @export var center_text: bool = true
 @export var amount: int = 1
 
 @onready var ref_card_name: RichTextLabel = $Name
 @onready var ref_card_text: RichTextLabel = $CardText
+@onready var ref_card_flavour: RichTextLabel = $Flavour
+@onready var ref_card_icon: TextureRect = $Image
 
 func _process(_delta) -> void:
 	ref_card_name.text = "[center]" + card_name + "[/center]"
+	ref_card_flavour.text = "[center][i]" + card_flavour + "[/i][/center]"
 	if center_text:
 		ref_card_text.text = "\n[center]" + card_text + "[/center]"
 	else:
 		ref_card_text.text = "[center]" + card_text + "[/center]"
+	ref_card_icon.texture = card_icon
--- logic/StudentCard.gd
+++ logic/StudentCard.gd
@@ -6,8 +6,12 @@ extends BasicCard
 
 @onready var psycha_value := $PanelPsycha/Psycha
 @onready var psycha_panel := $PanelPsycha
+@onready var text_panel := $PanelText
+@onready var background := $ColorBackground
 
 func _process(_delta):
 	super(_delta)
 	psycha_value.text = "[center]" + str(start_psycha_value) + "[/center]"
 	psycha_panel.self_modulate = student_color
+	text_panel.theme.bg_color = student_color
+#	background.self_modulate = student_color * Color.WHITE
